baf84441657362e46c7be8607e6f216cceda01be
Philipp <pd95@users.noreply.github.com>
2021-01-28 16:24:43 +0100

    Adding Toggle, Slider, Stepper and Pickers

--- Shared/BasicControlsScreen.swift
+++ Shared/BasicControlsScreen.swift
@@ -9,25 +9,32 @@ import SwiftUI
 
 struct BasicControlsScreen: View {
     
+    @State private var toggleValue = true
+    @State private var sliderValue = 15.0
+    @State private var stepperValue = 5
+    @State private var pickerValue: Screen? = .welcome
+    @State private var dateValue = Date(timeIntervalSinceReferenceDate: 0)
+    @State private var colorValue = Color.blue
+
     var body: some View {
         VStack {
+            HStack(alignment: .top) {
+                GroupBox(label: Label("Buttons & styles", systemImage: "square.grid.2x2")) {
+                    buttonStyles
+                }
+
+                GroupBox(label: Label("Controls & Pickers", systemImage: "square.grid.2x2")) {
+                    controlViews
+                }
+            }
+            
             HStack(alignment: .top) {
                 GroupBox(label: Label("Textfields", systemImage: "textbox")) {
                     textfields
                 }
-
+                
                 GroupBox(label: Label("Textfield styles", systemImage: "textbox")) {
                     textfieldStyles
-                }
-            }
-
-            HStack(alignment: .top) {
-                GroupBox(label: Label("Buttons, Controls & styles", systemImage: "square.grid.2x2")) {
-                    buttonStyles
-                }
-
-                GroupBox(label: Label("Controls", systemImage: "square.grid.2x2")) {
-                    controlViews
                 }
             }
         }
@@ -89,9 +96,45 @@ struct BasicControlsScreen: View {
         VStack(alignment: .leading, spacing: 10) {
             VStack(alignment: .labelAlignment, spacing: 10) {
 
-                LabeledControl(title: "NavigationLink:") {
-                    SecureField("Placeholder", text: .constant(""))
-                        .frame(maxWidth: 200)
+                LabeledControl(title: "Toggle:") {
+                    Toggle("Tick the box", isOn: $toggleValue)
+                }
+
+                LabeledControl(title: "Slider:") {
+                    Slider(value: $sliderValue, in: 0...30,
+                           minimumValueLabel: Text("0"),
+                           maximumValueLabel: Text("30")) {
+                        EmptyView()
+                    }
+                    .frame(maxWidth: 250)
+                }
+
+                LabeledControl(title: "Stepper:") {
+                    Stepper(value: $stepperValue, in: 0...15,
+                            step: 1) {
+                        Text("\(stepperValue)")
+                    }
+                }
+                
+                LabeledControl(title: "Picker:") {
+                    Picker("Screen", selection: $pickerValue) {
+                        ForEach(Screen.allCases, id: \.self) { screen in
+                            Text(screen.description)
+                        }
+                    }
+                    .frame(maxWidth: 250)
+                }
+
+                LabeledControl(title: "DatePicker:") {
+                    DatePicker("Start Date", selection: $dateValue)
+                        .frame(maxWidth: 250)
+                }
+
+                LabeledControl(title: "ColorPicker:") {
+                    ColorPicker("Text Color", selection: $colorValue)
+                        .alignmentGuide(.firstTextBaseline, computeValue: { dimension in
+                            dimension[VerticalAlignment.center] + 5
+                        })
                 }
             }
         }
